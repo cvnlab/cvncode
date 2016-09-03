345d87a89bfcbfd360652e9718c7ceda39c8a6bc
Kendrick Kay <kendrick@post.harvard.edu>
2016-09-03 12:29:03 -0500

    new

---
+++ utilities/fsconstants.m
@@ -0,0 +1,79 @@
+% 36 entries. note that the first entry is called 'unknown'.
+
+fslabels = {
+    'unknown'
+    'bankssts'
+    'caudalanteriorcingulate'
+    'caudalmiddlefrontal'  % #4
+    'corpuscallosum'
+    'cuneus'
+    'entorhinal'
+    'fusiform'
+    'inferiorparietal'
+    'inferiortemporal'
+    'isthmuscingulate'
+    'lateraloccipital'
+    'lateralorbitofrontal'
+    'lingual'
+    'medialorbitofrontal'
+    'middletemporal'
+    'parahippocampal'
+    'paracentral'
+    'parsopercularis'
+    'parsorbitalis'
+    'parstriangularis'
+    'pericalcarine'
+    'postcentral'
+    'posteriorcingulate'
+    'precentral'  % #25
+    'precuneus'
+    'rostralanteriorcingulate'
+    'rostralmiddlefrontal'
+    'superiorfrontal'
+    'superiorparietal'
+    'superiortemporal'
+    'supramarginal'
+    'frontalpole'
+    'temporalpole'
+    'transversetemporal'
+    'insula'
+}';
+
+fscolortable = [
+          25           5          25           0     1639705
+          25         100          40           0     2647065
+         125         100         160           0    10511485
+         100          25           0           0        6500
+         120          70          50           0     3294840
+         220          20         100           0     6558940
+         220          20          10           0      660700
+         180         220         140           0     9231540
+         220          60         220           0    14433500
+         180          40         120           0     7874740
+         140          20         140           0     9180300
+          20          30         140           0     9182740
+          35          75          50           0     3296035
+         225         140         140           0     9211105
+         200          35          75           0     4924360
+         160         100          50           0     3302560
+          20         220          60           0     3988500
+          60         220          60           0     3988540
+         220         180         140           0     9221340
+          20         100          50           0     3302420
+         220          60          20           0     1326300
+         120         100          60           0     3957880
+         220          20          20           0     1316060
+         220         180         220           0    14464220
+          60          20         220           0    14423100
+         160         140         180           0    11832480
+          80          20         140           0     9180240
+          75          50         125           0     8204875
+          20         220         160           0    10542100
+          20         180         140           0     9221140
+         140         220         220           0    14474380
+          80         160          20           0     1351760
+         100           0         100           0     6553700
+          70          20         170           0    11146310
+         150         150         200           0    13145750
+         255         192          32           0     2146559
+];
