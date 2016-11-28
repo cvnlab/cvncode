94f2b63d5f7ef933f941c468c840a9beacf80be0
Kendrick Kay <kendrick@post.harvard.edu>
2016-11-28 14:11:49 -0600

    major update for the new scheme with manual FS edits

--- cvnrunfreesurfer.m
+++ cvnrunfreesurfer.m
@@ -1,6 +1,6 @@
-function cvnrunfreesurfer(subjectid,dataloc,extraflags,scanstouse)
+function cvnrunfreesurfer(subjectid,dataloc,extraflags,scanstouse,t2nifti)
 
-% function cvnrunfreesurfer(subjectid,dataloc,extraflags,scanstouse)
+% function cvnrunfreesurfer(subjectid,dataloc,extraflags,scanstouse,t2nifti)
 %
 % <subjectid> is like 'C0001'
 % <dataloc> is:
@@ -11,9 +11,14 @@ function cvnrunfreesurfer(subjectid,dataloc,extraflags,scanstouse)
 % <scanstouse> (optional) is a vector of indices of T1 scans to use.
 %   For example, if there are 5 scans, [1 3 5] means to use the 1st, 3rd, and 5th.
 %   Default is to use all available.
+% <t2nifti> (optional) is a NIFTI T2 .nii.gz file. If you specify this case,
+%   <dataloc> must be case (2).
 %
 % push anatomical data through FreeSurfer.
 % see code for assumptions.
+%
+% history:
+% - 2016/11/28 - major update for the new scheme with manual FS edits.
 
 % input
 if ~exist('extraflags','var') || isempty(extraflags)
@@ -21,6 +26,9 @@ if ~exist('extraflags','var') || isempty(extraflags)
 end
 if ~exist('scanstouse','var') || isempty(scanstouse)
   scanstouse = [];
+end
+if ~exist('t2nifti','var') || isempty(t2nifti)
+  t2nifti = [];
 end
 
 % calc
@@ -34,7 +42,7 @@ assert(mkdir(dir0));
 if exist(dataloc,'dir')
 
   % figure out T1 files [ASSUME THAT THERE ARE AN EVEN NUMBER OF DIRECTORIES]
-  t1file = matchfiles(sprintf('%s/*T1w*',dataloc));
+  t1file = matchfiles(sprintf('%s/dicom/*T1w*',dataloc));
   assert(mod(length(t1file),2)==0);
   t1file = t1file(2:2:end);   % [hint: 2nd of each pair is the one that is homogenity-corrected]
 
@@ -45,17 +53,18 @@ if exist(dataloc,'dir')
 
   % convert dicoms to NIFTIs
   for p=1:length(t1file)
-    assert(0==unix(sprintf('dcm2nii -o %s -r N -x N %s',dir0,t1file{p})));
+    unix_wrapper(sprintf('dcm2nii -o %s -r N -x N %s',dir0,t1file{p}));
   end
         %   assert(0==unix(sprintf('dcm2nii -o %s -r N -x N %s',dir0,t2file)));
 
   % find the NIFTIs
-  t1nifti = matchfiles(sprintf('%s/*T1w*nii.gz',dir0));
+  t1nifti = matchfiles(sprintf('%s/dicom/*T1w*nii.gz',dir0));
         %   t2nifti = matchfiles(sprintf('%s/*T2w*nii.gz',dir0));
         %assert(length(t1nifti)==1);
         %   assert(length(t2nifti)==1);
         %t1nifti = t1nifti{1};
         %   t2nifti = t2nifti{1};
+  assert(isempty(t2nifti));
 
 % case 2
 else
@@ -65,13 +74,13 @@ else
   t1nifti = matchfiles(dataloc);
       %assert(length(t1nifti)==1);
       %t1nifti = t1nifti{1};
+  if ~isempty(t2nifti)
+    t2nifti = matchfiles(t2nifti);
+    assert(length(t2nifti)==1);
+    t2nifti = t2nifti{1};
+  end
   
 end
-
-    % do the reconstruction
-    % if exist('t2nifti','var')
-    %   assert(0==unix(sprintf('recon-all -s %s -i %s -T2 %s -T2pial -all %s > %s/reconlog.txt',subjectid,t1nifti,t2nifti,extraflags,dir0)));
-    % else
 
 % deal with scanstouse
 if isempty(scanstouse)
@@ -80,89 +89,12 @@ end
 
 % call recon-all
 str0 = catcell(2,cellfun(@(x) sprintf('-i %s ',x),t1nifti(scanstouse),'UniformOutput',0));  % make a string like '-i first -i second'
-assert(0==unix(sprintf('recon-all -s %s %s -all %s > %s/reconlog.txt',subjectid,str0,extraflags,dir0)));
-
-% end
-
-%%%%% convert T1 to NIFTI for external use
-
-assert(0==unix(sprintf('mri_convert %s/mri/T1.mgz %s/mri/T1.nii.gz',fsdir,fsdir)));
-
-%%%%% convert some miscellaneous files
-
-% convert .thickness files to ASCII
-assert(0==unix(sprintf('mris_convert -c %s/surf/lh.thickness %s/surf/lh.white %s/surf/lh.thickness.asc',fsdir,fsdir,fsdir)));
-assert(0==unix(sprintf('mris_convert -c %s/surf/rh.thickness %s/surf/rh.white %s/surf/rh.thickness.asc',fsdir,fsdir,fsdir)));
-
-% convert .curv files to ASCII
-assert(0==unix(sprintf('mris_convert -c %s/surf/lh.curv %s/surf/lh.white %s/surf/lh.curv.asc',fsdir,fsdir,fsdir)));
-assert(0==unix(sprintf('mris_convert -c %s/surf/rh.curv %s/surf/rh.white %s/surf/rh.curv.asc',fsdir,fsdir,fsdir)));
-
-%%%%% make mid-gray surface
-
-assert(0==unix(sprintf('mris_expand -thickness %s/surf/lh.white 0.5 %s/surf/lh.graymid',fsdir,fsdir)));
-assert(0==unix(sprintf('mris_expand -thickness %s/surf/rh.white 0.5 %s/surf/rh.graymid',fsdir,fsdir)));
-
-%%%%% consolidate mid-gray surface stuff into a .mat file
-
-prefixes = {'lh' 'rh'};
-for p=1:length(prefixes)
-  prefix0 = prefixes{p};
-
-  % read .graymid surface
-  [vertices,faces] = freesurfer_read_surf_kj(sprintf('%s/surf/%s.graymid',fsdir,prefix0));
-
-  % construct vertices (4 x V)
-  vertices = bsxfun(@plus,vertices',[128; 129; 128]);  % NOTICE THIS!!!
-  vertices(4,:) = 1;
-
-  % construct faces (F x 3)
-  faces = faces(:,[1 3 2]);  % necessary to convert freesurfer to matlab
-
-  % load auxiliary info (V x 1)
-  thickness = subscript(load(sprintf('%s/surf/%s.thickness.asc',fsdir,prefix0)),{':' 5});
-  curvature = subscript(load(sprintf('%s/surf/%s.curv.asc',fsdir,prefix0)),     {':' 5});
-
-  % get freesurfer labels (fslabels is V x 1)
-  [d,fslabels,colortable] = read_annotation(sprintf('%s/label/%s.aparc.annot',fsdir,prefix0),1);
-
-  % save
-  save(sprintf('%s/%s/%smidgray.mat',cvnpath('anatomicals',subjectid,prefix0), ...
-       'vertices','faces','thickness','curvature','fslabels');
-
+if isempty(t2nifti)
+  extrat2stuff = '';
+else
+  extrat2stuff = sprintf('-T2 %s -T2pial',t2nifti);
 end
+unix_wrapper(sprintf('recon-all -s %s %s %s -all %s > %s/reconlog.txt',subjectid,str0,extrat2stuff,extraflags,dir0));
 
-%%%%% calculate gray-matter information
-
-if isempty(regexp(extraflags,'hires'))
-
-  % load ribbon
-  [rib,M,mr_parms,volsz] = load_mgh(sprintf('%s/mri/ribbon.mgz',fsdir));
-  rib = fstoint(rib);
-
-  % load coordinates of surface vertices
-  coord0 = cat(2,loadmulti(sprintf('%s/%s/lhmidgray.mat',cvnpath('anatomicals'),subjectid),'vertices'), ...
-                 loadmulti(sprintf('%s/%s/rhmidgray.mat',cvnpath('anatomicals'),subjectid),'vertices'));
-
-  % compute distances to vertices [i.e. create a volume where gray matter voxels have certain informative values]
-  [dist,mnix] = surfaceslice2(ismember(rib,[3 42]),coord0,3,4);  % NOTICE HARD-CODED VALUES HERE
-
-  % save
-    % 1-mm volume with, for each gray matter voxel, distance to closest vertex (of mid-gray surface)
-  save_mgh(inttofs(dist),sprintf('%s/mri/ribbonsurfdist.mgz' ,fsdir),M,mr_parms);
-    % 1-mm volume with, for each gray matter voxel, index of closest vertex (of mid-gray surface)
-  save_mgh(inttofs(mnix),sprintf('%s/mri/ribbonsurfindex.mgz',fsdir),M,mr_parms);
-
-end
-
-%%%%% calculate transfer functions
-
-% calc
-[tfunFSSSlh,tfunFSSSrh,tfunSSFSlh,tfunSSFSrh] = ...
-  cvncalctransferfunctions([cvnpath('freesurfer') '/fsaverage/surf/lh.sphere.reg'], ...
-                           [cvnpath('freesurfer') '/fsaverage/surf/rh.sphere.reg'], ...
-                           sprintf('%s/%s/surf/lh.sphere.reg',cvnpath('freesurfer'),subjectid), ...
-                           sprintf('%s/%s/surf/rh.sphere.reg',cvnpath('freesurfer'),subjectid));
-
-% save
-save(sprintf('%s/%s/tfun.mat',cvnpath('anatomicals'),subjectid),'tfunFSSSlh','tfunFSSSrh','tfunSSFSlh','tfunSSFSrh');
+% convert T1 to NIFTI for external use
+unix_wrapper(sprintf('mri_convert %s/mri/T1.mgz %s/mri/T1.nii.gz',fsdir,fsdir));
---
+++ cvnrunfreesurfer2.m
@@ -0,0 +1,101 @@
+function cvnrunfreesurfer2(subjectid,extraflags)
+
+% function cvnrunfreesurfer2(subjectid,extraflags)
+%
+% <subjectid> is like 'C0001'
+% <extraflags> (optional) is a string with extra flags to pass to recon-all.
+%   Default: ''
+%
+% This is part 2/2 for pushing anatomical data through FreeSurfer.
+% see code for assumptions.
+
+% input
+if ~exist('extraflags','var') || isempty(extraflags)
+  extraflags = '';
+end
+
+% calc
+dir0 = sprintf('%s/%s',cvnpath('anatomicals'),subjectid);
+fsdir = sprintf('%s/%s',cvnpath('freesurfer'),subjectid);
+
+% make subject anatomical directory
+assert(mkdir(dir0));
+
+%%%%% convert some miscellaneous files
+
+% convert .thickness files to ASCII
+unix_wrapper(sprintf('mris_convert -c %s/surf/lh.thickness %s/surf/lh.white %s/surf/lh.thickness.asc',fsdir,fsdir,fsdir));
+unix_wrapper(sprintf('mris_convert -c %s/surf/rh.thickness %s/surf/rh.white %s/surf/rh.thickness.asc',fsdir,fsdir,fsdir));
+
+% convert .curv files to ASCII
+unix_wrapper(sprintf('mris_convert -c %s/surf/lh.curv %s/surf/lh.white %s/surf/lh.curv.asc',fsdir,fsdir,fsdir));
+unix_wrapper(sprintf('mris_convert -c %s/surf/rh.curv %s/surf/rh.white %s/surf/rh.curv.asc',fsdir,fsdir,fsdir));
+
+%%%%% make mid-gray surface
+
+unix_wrapper(sprintf('mris_expand -thickness %s/surf/lh.white 0.5 %s/surf/lh.graymid',fsdir,fsdir));
+unix_wrapper(sprintf('mris_expand -thickness %s/surf/rh.white 0.5 %s/surf/rh.graymid',fsdir,fsdir));
+
+%%%%% consolidate mid-gray surface stuff into a .mat file
+
+prefixes = {'lh' 'rh'};
+for p=1:length(prefixes)
+  prefix0 = prefixes{p};
+
+  % read .graymid surface
+  [vertices,faces] = freesurfer_read_surf_kj(sprintf('%s/surf/%s.graymid',fsdir,prefix0));
+
+  % construct vertices (4 x V)
+  vertices = bsxfun(@plus,vertices',[128; 129; 128]);  % NOTICE THIS!!!
+  vertices(4,:) = 1;
+
+  % construct faces (F x 3)
+  faces = faces(:,[1 3 2]);  % necessary to convert freesurfer to matlab
+
+  % load auxiliary info (V x 1)
+  thickness = subscript(load(sprintf('%s/surf/%s.thickness.asc',fsdir,prefix0)),{':' 5});
+  curvature = subscript(load(sprintf('%s/surf/%s.curv.asc',fsdir,prefix0)),     {':' 5});
+
+  % get freesurfer labels (fslabels is V x 1)
+  [d,fslabels,colortable] = read_annotation(sprintf('%s/label/%s.aparc.annot',fsdir,prefix0),1);
+
+  % save
+  save(sprintf('%s/%s/%smidgray.mat',cvnpath('anatomicals'),subjectid,prefix0), ...
+       'vertices','faces','thickness','curvature','fslabels');
+
+end
+
+%%%%% calculate gray-matter information
+
+if isempty(regexp(extraflags,'hires'))
+
+  % load ribbon
+  [rib,M,mr_parms,volsz] = load_mgh(sprintf('%s/mri/ribbon.mgz',fsdir));
+  rib = fstoint(rib);
+
+  % load coordinates of surface vertices
+  coord0 = cat(2,loadmulti(sprintf('%s/%s/lhmidgray.mat',cvnpath('anatomicals'),subjectid),'vertices'), ...
+                 loadmulti(sprintf('%s/%s/rhmidgray.mat',cvnpath('anatomicals'),subjectid),'vertices'));
+
+  % compute distances to vertices [i.e. create a volume where gray matter voxels have certain informative values]
+  [dist,mnix] = surfaceslice2(ismember(rib,[3 42]),coord0,3,4);  % NOTICE HARD-CODED VALUES HERE
+
+  % save
+    % 1-mm volume with, for each gray matter voxel, distance to closest vertex (of mid-gray surface)
+  save_mgh(inttofs(dist),sprintf('%s/mri/ribbonsurfdist.mgz' ,fsdir),M,mr_parms);
+    % 1-mm volume with, for each gray matter voxel, index of closest vertex (of mid-gray surface)
+  save_mgh(inttofs(mnix),sprintf('%s/mri/ribbonsurfindex.mgz',fsdir),M,mr_parms);
+
+end
+
+%%%%% calculate transfer functions
+
+% calc
+[tfunFSSSlh,tfunFSSSrh,tfunSSFSlh,tfunSSFSrh] = ...
+  cvncalctransferfunctions([cvnpath('freesurfer') '/fsaverage/surf/lh.sphere.reg'], ...
+                           [cvnpath('freesurfer') '/fsaverage/surf/rh.sphere.reg'], ...
+                           sprintf('%s/%s/surf/lh.sphere.reg',cvnpath('freesurfer'),subjectid), ...
+                           sprintf('%s/%s/surf/rh.sphere.reg',cvnpath('freesurfer'),subjectid));
+
+% save
+save(sprintf('%s/%s/tfun.mat',cvnpath('anatomicals'),subjectid),'tfunFSSSlh','tfunFSSSrh','tfunSSFSlh','tfunSSFSrh');
