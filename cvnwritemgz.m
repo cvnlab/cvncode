ba95e1c6946bdf21ca0f2c3df20422e8d5aac8fb
Kendrick Kay <kendrick@post.harvard.edu>
2016-11-30 13:09:20 -0600

    minor fix

--- cvnwritemgz.m
+++ cvnwritemgz.m
@@ -21,6 +21,9 @@ if ~exist('outputdir','var') || isempty(outputdir)
   outputdir = sprintf('%s/surf',fsdir);
 end
 
+% prep
+mkdirquiet(outputdir);
+
 % load template
 file0 = sprintf('%s/surf/%s.w-g.pct.mgh',fsdir,hemi(1:2));
 if ~exist(file0,'file')  % fsaverage doesn't have the above file, so let's use this one:
