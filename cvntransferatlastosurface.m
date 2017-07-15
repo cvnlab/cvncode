cf05e2eba48745651c4f6e0cc66d2c28d35b5138
Kendrick Kay <kendrick@post.harvard.edu>
2017-07-15 16:14:04 -0500

    allow non-dense processing

--- cvntransferatlastosurface.m
+++ cvntransferatlastosurface.m
@@ -6,7 +6,8 @@ function cvntransferatlastosurface(subjectid,fsmap,hemi,outpre,fstruncate,fun,ou
 % <fsmap> is the fsaverage surface file like '/software/freesurfer/fsaveragemaps/KayDataFFA1-RH.mgz'
 % <hemi> is 'lh' or 'rh' indicating whether the surface file is left or right hemisphere
 % <outpre> is the prefix of the destination .mgz files to write, like 'KayDataFFA1'
-% <fstruncate> is the name of the truncation surface in fsaverage
+% <fstruncate> is the name of the truncation surface in fsaverage.
+%   if [], this indicates the non-dense processing case.
 % <fun> (optional) is a function to apply to <fsmap> after loading it.
 %   Default is to do nothing (use values as-is).
 % <outputdir> (optional) is the directory to write the .mgz files to.
@@ -19,6 +20,8 @@ function cvntransferatlastosurface(subjectid,fsmap,hemi,outpre,fstruncate,fun,ou
 % (1) <hemi>.<outpre>.mgz - standard (non-dense) surface
 % (2) <hemi>DENSE.<outpre>.mgz - dense surface
 % (3) <hemi>DENSETRUNC<fstruncate>.<outpre>.mgz - dense, truncated surface
+%
+% Note that (2) and (3) are not written if <fstruncate> is [].
 
 % NOTES FROM KEITH:
 % Cvnroimask looks for label/rh[DENSE|DENSETRUNCpt].roiname.(mgz|mgh|label|annot)
@@ -42,10 +45,14 @@ end
 
 % load transfer functions
 a1 = load(sprintf('%s/%s/tfun.mat',     cvnpath('anatomicals'),subjectid));
-a2 = load(sprintf('%s/%s/tfunDENSE.mat',cvnpath('anatomicals'),subjectid));
 
-% load truncation indices
-a3 = load(sprintf('%s/surf/%s.DENSETRUNC%s.mat',fsdir,hemi,fstruncate));  % contains 'validix'
+% load more
+if ~isempty(fstruncate)
+  a2 = load(sprintf('%s/%s/tfunDENSE.mat',cvnpath('anatomicals'),subjectid));
+
+  % load truncation indices
+  a3 = load(sprintf('%s/surf/%s.DENSETRUNC%s.mat',fsdir,hemi,fstruncate));  % contains 'validix'
+end
 
 % load fsaverage map
 vals = flatten(load_mgh(fsmap));  % 1 x 163842
@@ -75,15 +82,19 @@ cvnwritemgz(subjectid,outpre,vals0,hemi,outputdir);
 
 %%%%% DENSE CASES (DENSE ON THE SPHERE; ALSO TRUNCATED VERSION)
 
-% transfer to single subject space (using nearest neighbor interpolation)
-if isequal(hemi,'rh')
-  vals0 = a2.tfunFSSSrh(vals);
-else
-  vals0 = a2.tfunFSSSlh(vals);
+if ~isempty(fstruncate)
+
+  % transfer to single subject space (using nearest neighbor interpolation)
+  if isequal(hemi,'rh')
+    vals0 = a2.tfunFSSSrh(vals);
+  else
+    vals0 = a2.tfunFSSSlh(vals);
+  end
+
+  % write mgz
+  cvnwritemgz(subjectid,outpre,vals0,[hemi 'DENSE'],outputdir);
+
+  % write mgz (truncated)
+  cvnwritemgz(subjectid,outpre,vals0(a3.validix),[hemi 'DENSETRUNC' fstruncate],outputdir);
+
 end
-
-% write mgz
-cvnwritemgz(subjectid,outpre,vals0,[hemi 'DENSE'],outputdir);
-
-% write mgz (truncated)
-cvnwritemgz(subjectid,outpre,vals0(a3.validix),[hemi 'DENSETRUNC' fstruncate],outputdir);
