bba0986969b7d2102d46623cec234b67b29fba6d
Kendrick Kay <kendrick@post.harvard.edu>
2017-08-25 08:36:58 -0500

    remove FIR-related stuff and only do some selected viewpoints

--- cvnvisualizestandardglm.m
+++ cvnvisualizestandardglm.m
@@ -1,6 +1,6 @@
-function cvnvisualizestandardglm(subjectid,numlayers,layerprefix,fstruncate,datadir,outputdir,assumedir,firdir)
+function cvnvisualizestandardglm(subjectid,numlayers,layerprefix,fstruncate,datadir,outputdir,assumedir)
 
-% function cvnvisualizestandardglm(subjectid,numlayers,layerprefix,fstruncate,datadir,outputdir)
+% function cvnvisualizestandardglm(subjectid,numlayers,layerprefix,fstruncate,datadir,outputdir,assumedir)
 %
 % <subjectid> is like 'C0001'
 % <numlayers> is like 6
@@ -11,21 +11,23 @@ function cvnvisualizestandardglm(subjectid,numlayers,layerprefix,fstruncate,data
 % <outputdir> is like
 %   '/home/stone/generic/Dropbox/cvnlab/ppresults/CVNS003/glmviz/20160628-CVNS003-fLoc_ph'
 % <assumedir> is like 'GLMCS_floc_assume'
-% <firdir> is like 'GLM_FIR_Standard'
 %
 % For a number of different views, write out figures showing a variety of quantities related
-% to the GLM results in <datadir> (both assumedir and firdir).
+% to the GLM results in <datadir>/<assumedir>.
 %
 % These figures pertain to:
 % (1) from the condition-split GLM: R2 values, beta weights and their errors
-% (2) from the FIR GLM: estimates of peak time
-% (3) the visually responsive ROI that is manually defined based on the condition-split GLM R2
-%
-% We also create some non-map-based figures pertaining to
-% R2 values, the PCA analysis, and the peak times.
+% (2) the visually responsive ROI that is manually defined based on the condition-split GLM R2
 %
 % history:
+% - 2017/08/25 - remove FIR-related stuff and only do some selected viewpoints
 % - 2017/08/14 - add support for flat.patch
+
+% REMOVED / STALE:
+% <firdir> is like 'GLM_FIR_Standard'
+% (2) from the FIR GLM: estimates of peak time
+% We also create some non-map-based figures pertaining to
+% R2 values, the PCA analysis, and the peak times.
 
 %%%%%%%%% setup
 
@@ -69,28 +71,29 @@ vrroi = loadmulti([datadir '/' assumedir '/ROIs/?h.VIS_RESP_thresh10.mat'],'R',1
 valid = loadmulti(sprintf([datadir '/preprocessVER1SURF%s/valid.mat'],subjectid),'data');  % 1 x 6 x vertices
 valid = logical(squish(permute(valid,[3 2 1]),2));  % vertices*6 x 1
 
-% load PCA results
-a1 = load([datadir '/' firdir '/results/pca.mat']);
-
-% prep peaktime
-peaktime = reshape(copymatrix(zeros(size(valid)),valid,a1.peaktime),[],numlayers);  % vertices x 6
+% STALE
+% % load PCA results
+% a1 = load([datadir '/' firdir '/results/pca.mat']);
+% 
+% % prep peaktime
+% peaktime = reshape(copymatrix(zeros(size(valid)),valid,a1.peaktime),[],numlayers);  % vertices x 6
 
 %%%%%%%%% proceed
 
 % define
 allviews = { ...
-  {'ventral'        'sphere'                   0 1000    0         [1 1]} ...
-  {'occip'          'sphere'                   0 1000    0         [1 1]} ...
-  {'occip'          'inflated'                 0  500    0         [1 1]} ...
-  {'ventral'        'inflated'                 1  500    0         [1 1]} ...
-  {'parietal'       'inflated'                 0  500    0         [1 1]} ...
-  {'medial'         'inflated'                 0  500    0         [1 1]} ...
-  {'lateral'        'inflated'                 0  500    0         [1 1]} ...
-  {'medial-ventral' 'inflated'                 0  500    0         [1 1]} ...
-  {'occip'          'sphere'                   0 1000    1         [1 1]} ...
-  {'ventral'        'inflated'                 1  500    1         [1 1]} ...
-  {'ventral'        'gVTC.flat.patch.3d'       1 2000    0         [160 0]} ...   % 12.5 pixels per mm
-  {''               'gEVC.flat.patch.3d'       0 1500    0         [120 0]} ...   % 12.5 pixels per mm
+...%  {'ventral'        'sphere'                   0 1000    0         [1 1]} ...
+   {'occip'          'sphere'                   0 1000    0         [1 1]} ...
+...%  {'occip'          'inflated'                 0  500    0         [1 1]} ...
+...%  {'ventral'        'inflated'                 1  500    0         [1 1]} ...
+...%  {'parietal'       'inflated'                 0  500    0         [1 1]} ...
+...%  {'medial'         'inflated'                 0  500    0         [1 1]} ...
+...%  {'lateral'        'inflated'                 0  500    0         [1 1]} ...
+...%  {'medial-ventral' 'inflated'                 0  500    0         [1 1]} ...
+   {'occip'          'sphere'                   0 1000    1         [1 1]} ...
+...%  {'ventral'        'inflated'                 1  500    1         [1 1]} ...
+   {'ventral'        'gVTC.flat.patch.3d'       1 2000    0         [160 0]} ...   % 12.5 pixels per mm
+   {''               'gEVC.flat.patch.3d'       0 1500    0         [120 0]} ...   % 12.5 pixels per mm
 };
 
 % loop over views
@@ -202,58 +205,61 @@ for zz=1:length(allviews)
   [roiimg,~,rgbimg] = writefun(double(vflatten(vrroi)), ...
     sprintf('vrroi.png'),'gray',[0 1],0.5,0.7);
 
-  % peak time
-  for pp=1:numlayers
-    writefun(double(vflatten(peaktime(:,pp))), ...
-      sprintf('peaktime_layer%s.png',extnames{pp}),'jet',[4 10],[],[]);
-  end
+% STALE
+%   % peak time
+%   for pp=1:numlayers
+%     writefun(double(vflatten(peaktime(:,pp))), ...
+%       sprintf('peaktime_layer%s.png',extnames{pp}),'jet',[4 10],[],[]);
+%   end
 
   %%%%% proceed to non-maps
   
   if zz==1
 
-    % hist of R2
-    figureprep([100 100 800 800]); hold on;
-    hist(allR2(~weird1),-4:2:100);
-    xlabel('R2');
-    ax = axis; axis([-4 100 ax(3:4)]);
-    figurewrite('histR2',[],[],outputdir);
+% WELL, DO WE NEED THIS
+%     % hist of R2
+%     figureprep([100 100 800 800]); hold on;
+%     hist(allR2(~weird1),-4:2:100);
+%     xlabel('R2');
+%     ax = axis; axis([-4 100 ax(3:4)]);
+%     figurewrite('histR2',[],[],outputdir);
 
-    % PC timecourses
-    numtoplot = 5;
-    figureprep([100 100 700 500]);
-    h = plot(a1.v(:,1:numtoplot));
-    legend(h,'Location','EastOutside');
-    set(straightline(0,'h','k-'),'LineWidth',2);
-    xlabel('time points (raw)');
-    figurewrite('PC_timecourses',[],[],outputdir);
-    
-    % PC eigenvalues
-    figureprep([100 100 500 500]);
-    plot(diag(a1.s),'ro-');
-    xlabel('PC number');
-    ylabel('Eigenvalue');
-    figurewrite('PC_eigenvalues',[],[],outputdir);
-
-    % hist of peaktime
-    figureprep([100 100 600 600]);
-    hist(a1.peaktime(:),1000);
-    xlabel('Peak time (s)');
-    figurewrite('histpeaktime',[],[],outputdir);
-
-    % scatter R2 vs peaktime
-    figureprep([100 100 600 600]); hold on;
-    xxx = allR2(valid);
-    yyy = a1.peaktime(:);
-    bbx = linspace(0,80,100);
-    bby = 0:0.1:15;
-    [n,x,y] = hist2d(xxx,yyy,bbx,bby);
-    imagesc(x(1,:),y(:,1),log(n));
-    colormap(jet);
-    xlabel('R2')
-    ylabel('Peak time (s)');
-    axis([0 80 0 15]);
-    figurewrite('R2_vs_peaktime',[],[],outputdir);
+% STALE
+%     % PC timecourses
+%     numtoplot = 5;
+%     figureprep([100 100 700 500]);
+%     h = plot(a1.v(:,1:numtoplot));
+%     legend(h,'Location','EastOutside');
+%     set(straightline(0,'h','k-'),'LineWidth',2);
+%     xlabel('time points (raw)');
+%     figurewrite('PC_timecourses',[],[],outputdir);
+%     
+%     % PC eigenvalues
+%     figureprep([100 100 500 500]);
+%     plot(diag(a1.s),'ro-');
+%     xlabel('PC number');
+%     ylabel('Eigenvalue');
+%     figurewrite('PC_eigenvalues',[],[],outputdir);
+% 
+%     % hist of peaktime
+%     figureprep([100 100 600 600]);
+%     hist(a1.peaktime(:),1000);
+%     xlabel('Peak time (s)');
+%     figurewrite('histpeaktime',[],[],outputdir);
+% 
+%     % scatter R2 vs peaktime
+%     figureprep([100 100 600 600]); hold on;
+%     xxx = allR2(valid);
+%     yyy = a1.peaktime(:);
+%     bbx = linspace(0,80,100);
+%     bby = 0:0.1:15;
+%     [n,x,y] = hist2d(xxx,yyy,bbx,bby);
+%     imagesc(x(1,:),y(:,1),log(n));
+%     colormap(jet);
+%     xlabel('R2')
+%     ylabel('Peak time (s)');
+%     axis([0 80 0 15]);
+%     figurewrite('R2_vs_peaktime',[],[],outputdir);
 
   end
 
