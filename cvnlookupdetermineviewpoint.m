068bf5b6eb32de79ce5169c32aa935c41a4b21b6
Kendrick Kay <kendrick@post.harvard.edu>
2023-03-14 16:32:25 -0500

    new

--- cvnlookupdetermineviewpoint.m
+++ cvnlookupdetermineviewpoint.m
@@ -1,6 +1,7 @@
-%%% NOTE: this is a set of notes and still a work in progress.
+% This script can be used to help determine new viewpoints to be 
+% used in cvnlookup.m and cvndefinerois.m.
 
-% show the fsaverage surface
+% load surface
 surf = cvnreadsurface('fsaverage','lh','inflated','orig');
 curv = cvnreadsurfacemetric('fsaverage','lh','curv',[],'orig');
 surf = cvnreadsurface('fsaverage','rh','inflated','orig');
@@ -13,53 +14,29 @@ set(h,'EdgeColor','none');
 %%set(h,'FaceColor',[1 .5 .5]);
 set(h,'FaceVertexCData',double(curv<0));
 set(h,'FaceColor','interp');
-%set(h,'FaceColor',[1 .5 .5]);
-lighting gouraud
+lighting gouraud;
 camlight;
 axis equal;
 
-% rotate a little manually.
-[az,el]=view
-view(180,90);
-    %az = 95; el = -60;
-    % az = -120; el = -5
-    % az =  120; el = -5
-view(-35,40);  %lh
-view( 35,40);  %rh
+% rotate manually
 
-% check parameters:
-view(-120,-5);  %lh
-view( 120,-5);  %rh
-view(180,90);  % rh
+% get the current view (azimuth, elevation)
+[az,el] = view
 
-% define
-oo = [az el 270];  % value on 3 means CW
-oo = [-120 -5 0];
-oo = [ 120 -5 0];
-oo = [0 90 0];
-oo = [0 90 0];
-oo = [-180 0 0];
-oo = [-180 0 0];
-oo = [-35 40 0];
-oo = [ 35 40 0];
+% explicitly set the view
+view(-35,40);  % lh
+view( 35,40);  % rh
 
-% check the 3rd parameter (rotation)
-figure(1); view(oo(1),oo(2));
-%camlight;
-figurewrite('test',[],[],pwd,1); 
-im = imread('test.png');
-figure(2); setfigurepos([100 100 900 900]); imagesc(imrotate(im,-oo(3)));
+% define 3-element vector with [A E R]
+% where A is azimuth
+%       E is elevation
+%       R is image rotation (positive means clockwise)
+oo = [-35 40 0];  % lh
+oo = [ 35 40 0];  % rh
 
-% modify code (cvn functions) accordingly.
-
-%%%% other junk
-
-% some tests.
-cvnlookup('EMILY001', 12,[],[],[],10,[],[],{'savelookup',false});   % savelookup false
-cvnlookup('fsaverage', 14,[],[],[],10);
-cvnlookup('fsaverage', 15,[],[],[],10);
-
-for p=1:10
-  cvnlookup(sprintf('EMILY%03d',p), 12,[],[],[],10,[],[],{'savelookup',false});   % savelookup
-  imwrite(rgbimg,sprintf('~/Dropbox/pitchprf/subj%03d.png',p));
-end
+% after you carefully decide on your parameters,
+% the code (cvn functions) need to be modified
+% accordingly.
+%
+% the relevant functions are:
+%   cvnlookup.m, cvnlookupviewpoint.m, cvndefinerois.m
