1676ced9860be682c994b7b23c568a1415811bc6
Kendrick Kay <kendrick@post.harvard.edu>
2026-01-05 09:02:45 -0600

    add <dcmcmd>

--- cvncollectT1s.m
+++ cvncollectT1s.m
@@ -1,6 +1,6 @@
-function files = cvncollectT1s(subjectid,dataloc,gradfile,str0,wantskip)
+function files = cvncollectT1s(subjectid,dataloc,gradfile,str0,wantskip,dcmcmd)
 
-% function files = cvncollectT1s(subjectid,dataloc,gradfile,str0,wantskip)
+% function files = cvncollectT1s(subjectid,dataloc,gradfile,str0,wantskip,dcmcmd)
 %
 % <subjectid> is like 'C0001' or {A} where A is the anatomicals directory to write to
 % <dataloc> is a scan directory like '/home/stone-ext1/fmridata/20151014-ST001-wynn,subject1'
@@ -11,6 +11,8 @@ function files = cvncollectT1s(subjectid,dataloc,gradfile,str0,wantskip)
 % <wantskip> (optional) is whether to treat as pairs and use the second of each pair.
 %   (The idea is that the second of each pair might be homogeneity-corrected.)
 %   Default: 1.
+% <dcmcmd> (optional) is the dcm2nii command line call to use (excluding 
+%   the -o output and the input file). Default: 'dcm2nii -r N -x N'.
 %
 % Within the specified scan directories (in the order as given), 
 % find all of the T1 (or whatever) DICOM directories, and if <wantskip>,
@@ -41,6 +43,9 @@ if ~exist('str0','var') || isempty(str0)
 end
 if ~exist('wantskip','var') || isempty(wantskip)
   wantskip = 1;
+end
+if ~exist('dcmcmd','var') || isempty(dcmcmd)
+  dcmcmd = 'dcm2nii -r N -x N';
 end
 
 % calc
@@ -83,9 +88,19 @@ end
 % convert dicoms to NIFTIs and get the filenames
 files = {};
 for p=1:length(t1files)
-  result = unix_wrapper(sprintf('dcm2nii -o %s -r N -x N %s',dir0,t1files{p}));
+  result = unix_wrapper(sprintf('%s -o %s %s',dcmcmd,dir0,t1files{p}));
+  
+  % in some cases, this may indicate the output file
   temp = regexp(result,'GZip\.\.\.(.+\.gz)','tokens');
-  files{p} = [dir0 '/' temp{1}{1}];
+  if ~isempty(temp)
+    files{p} = [dir0 '/' temp{1}{1}];
+  % otherwise, let's try it this way
+  else
+    temp = matchfiles([dir0 '/' stripfile(t1files{p},1) '*.nii*']);
+    assert(length(temp)==1);
+    files{p} = temp{1};
+  end
+
 end
 
 % get rid of dumb whitespace from filenames
